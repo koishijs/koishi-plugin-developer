b7ec7a1711e9128a31a382a88a01137db20a8fda
yijie4188 <yijie4188@gmail.com>
2021-05-05 18:50:06 +0800

    fix(cli): account uni type dump to str

--- cli/run-gocq.ts
+++ cli/run-gocq.ts
@@ -4,7 +4,6 @@ import yaml from 'js-yaml'
 import commander from 'commander'
 import { spawn } from 'child_process'
 import { merge } from 'koishi'
-import * as readline from 'readline'
 import inquirer from 'inquirer'
 
 const program = new commander.Command()
@@ -31,65 +30,68 @@ const filePath = new Proxy({
   envTempConfig: '.env.temp.yml'
 }, {
   get(target, p, receiver) {
-    return path.resolve(
-      process.cwd(), target[p]
-    )
+    return target[p]
+    // return path.resolve(
+    //   process.cwd(), target[p]
+    // )
   }
 })
 
-const args = []
 ;(async () => {
-  if (!fs.existsSync(filePath['optionConfig'])) {
-    throw new Error('Config file can not found.')
-  }
-  if (!fs.existsSync(filePath['envConfig'])) {
-    const defaultEnvConfig = {
-      middlewares: { 'access-token': '74dc0131-0bce-4a39-8ee8-21fea81a892b' },
-      account: {
-        uin: '', pwd: ''
+  const args = []
+  await (async function generateTempEnvYaml () {
+    if (!fs.existsSync(filePath['optionConfig'])) {
+      throw new Error('Config file can not found.')
+    }
+    if (!fs.existsSync(filePath['envConfig'])) {
+      const defaultEnvConfig = {
+        middlewares: { 'access-token': '74dc0131-0bce-4a39-8ee8-21fea81a892b' },
+        account: {
+          uin: undefined, pwd: ''
+        }
       }
+      const answer: {
+        isAutoCreate: boolean
+        uni: number
+        pwd: string
+      } = await inquirer
+        .prompt([{
+          type: 'confirm',
+          name: 'isAutoCreate',
+          message: '.env.yml 文件不存在，是否自动创建新文件'
+        }, {
+          type: 'input',
+          name: 'uni',
+          message: '请输入 bot qq账号'
+        }, {
+          type: 'input',
+          name: 'pwd',
+          message: '请输入 bot 密码(为空时扫码登陆)'
+        }])
+      if (!answer.isAutoCreate) {
+        throw new Error('Env Config file can not found, You need to create a `.env.yml` file in the root directory.')
+      }
+      defaultEnvConfig.account.uin = +answer.uni
+      defaultEnvConfig.account.pwd = answer.pwd
+      fs.writeFileSync(
+        filePath['envConfig'], yaml.dump(defaultEnvConfig)
+      )
     }
-    const answer: {
-      isAutoCreate: boolean
-      uni: string
-      pwd: string
-    } = await inquirer
-      .prompt([{
-        type: 'confirm',
-        name: 'isAutoCreate',
-        message: '.env.yml 文件不存在，是否自动创建新文件'
-      }, {
-        type: 'input',
-        name: 'uni',
-        message: '请输入 bot qq账号'
-      }, {
-        type: 'input',
-        name: 'pwd',
-        message: '请输入 bot 密码(为空时扫码登陆)'
-      }])
-    if (!answer.isAutoCreate) {
-      throw new Error('Env Config file can not found, You need to create a `.env.yml` file in the root directory.')
-    }
-    defaultEnvConfig.account.uin = answer.uni
-    defaultEnvConfig.account.pwd = answer.pwd
+
+    const optionConfig = yaml.load(
+      fs.readFileSync(filePath['optionConfig']).toString()
+    ) as {}
+    const tempYaml = yaml.dump(merge(
+      yaml.load(
+        fs.readFileSync(filePath['envConfig']).toString()
+      ) as {}, optionConfig
+    ))
     fs.writeFileSync(
-      filePath['envConfig'], yaml.dump(defaultEnvConfig)
+      filePath['envTempConfig'], tempYaml
     )
-  }
+  })()
 
-  const optionConfig = yaml.load(
-    fs.readFileSync(filePath['optionConfig']).toString()
-  ) as {}
-  const tempYaml = yaml.dump(merge(
-    yaml.load(
-      fs.readFileSync(filePath['envConfig']).toString()
-    ) as {}, optionConfig
-  ))
-  fs.writeFileSync(
-    filePath['envTempConfig'], tempYaml
-  )
   args.push('-c', filePath['envTempConfig'])
-
   console.log(
     `> ${filePath['gocqExe']} ${args.join(' ')}`
   )
