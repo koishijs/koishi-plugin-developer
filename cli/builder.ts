32d08fc06669816db6ed80a92be14ad19a2de3ee
yijie4188 <yijie4188@gmail.com>
2021-05-06 17:47:30 +0800

    feat(cli): improve the builder script

--- cli/builder.ts
+++ cli/builder.ts
@@ -1,47 +1,120 @@
-import { spawn } from 'child_process'
+import { spawn, SpawnOptionsWithoutStdio } from 'child_process'
 import inquirer from 'inquirer'
+import fs from 'fs'
+import commander from 'commander'
 
-interface Answer {
-  pluginName: string
+import pkg from '../package.json'
+
+interface Options {
+  clear?: boolean
+  all?: boolean
+  plugins?: string[]
+  format?: 'esm' | 'umd' | 'min'
+  allFormat?: boolean
 }
 
-const rollupBuild = (argv: any[], answer: Answer) => {
-    let [ cmd, args ] = ['rollup', [
-      '-c', ...argv
-    ]]
-    if (process.platform === 'win32') {
-      cmd += '.cmd'
+const doCommand = (
+  cmd: string, args?: ReadonlyArray<string>, options?: SpawnOptionsWithoutStdio
+) => new Promise<number>(resolve => {
+  if (process.platform === 'win32') {
+    cmd += '.cmd'
+  }
+  const execCmd = spawn(cmd, args, options)
+  execCmd.stdout.pipe(process.stdout)
+  execCmd.stderr.pipe(process.stderr)
+  execCmd.on('exit', code => {
+    resolve(code)
+  })
+})
+
+const runRollup = (args: any[], options: SpawnOptionsWithoutStdio = {}) => new Promise(resolve => {
+  args = [ '-c', ...args ]
+  const cmd = 'rollup'
+  const cwd = options.cwd ?? process.cwd()
+
+  console.log('working dir:', cwd)
+  console.log(
+    `> ${cmd} ${args.join(' ')}`
+  )
+  resolve(doCommand(cmd, args, { ...options, cwd }))
+})
+
+const buildPlugins = async (opts: Options) => {
+  for (let i = 0;i < opts.plugins.length; i++) {
+    const plugin = opts.plugins[i]
+    const rollupOptions = { cwd: `./packages/plugin-${plugin}` }
+    if (opts.format) {
+      if (![ 'esm', 'umd', 'min' ].includes(opts.format)) throw new Error(
+        'Format param is not found.'
+      )
+      await runRollup([  '--environment', `FORMAT:${opts.format}` ], rollupOptions)
+      return
     }
-    const cwd = `./packages/plugin-${answer.pluginName}`
-
-    console.log('working dir:', cwd)
-    console.log(
-      `> ${cmd} ${args.join(' ')}`
-    )
-
-    const execCmd = spawn(cmd, args, { cwd })
-    execCmd.stdout.pipe(process.stdout)
-    execCmd.stderr.pipe(process.stderr)
-    execCmd.on('exit', code => {
-      console.log('Exit code:', code)
-    })
+    if (opts.allFormat) {
+      const promises = [
+        ...[ 'esm', 'umd', 'min' ]
+          .map(format => [ '--environment', `FORMAT:${format}` ])
+          .map(argv => runRollup(argv, rollupOptions))
+      ]
+      await Promise.all(promises)
+      return
+    }
+  }
 }
+
+const program = new commander.Command(
+  'builder'
+).version(
+  pkg.version
+).option(
+  '--clear', 'clear build data.'
+).option(
+  '--all', 'build all plugins.'
+).option(
+  '--format', 'build target format of plugin.', 'esm'
+).option(
+  '--plugins [plugins...]', 'build target plugins.'
+).option(
+  '--all-format', 'build plugin all format.'
+).parse(process.argv)
 
 ;(async () => {
-  const argv = process.argv.splice(2)
+  const opts = program.opts() as Options
 
-  const answer: Answer = await inquirer
-    .prompt([{
+  const answer: {
+    pluginName?: string
+  } = await (async () => {
+    if (!opts.plugins) opts.plugins = []
+
+    return opts.all || opts.plugins.length > 0 ? {} : inquirer.prompt([{
       type: 'input',
       name: 'pluginName',
-      message: '请输入插件名'
+      message: '请输入插件名',
+      validate(input: string) {
+        if (
+          !fs.existsSync(`./packages/plugin-${input}`)
+        ) return 'Plugin dir is not found.'
+        return true
+      }
     }])
-
-  if (argv[0] !== 'all') {
-    rollupBuild(argv, answer)
-  } else {
-    rollupBuild([ '--environment', 'FORMAT:esm' ], answer)
-    rollupBuild([ '--environment', 'FORMAT:umd' ], answer)
-    rollupBuild([ '--environment', 'FORMAT:min' ], answer)
+  })()
+  if (opts.clear) {
+    const clearPath = `packages/plugin-${ opts.all ? '*' : answer.pluginName }/dist`
+    await doCommand('rimraf', [ clearPath ])
   }
-})()
+
+  if (!opts.all) {
+    answer.pluginName && opts.plugins.push(answer.pluginName)
+    await buildPlugins(opts)
+  } else {
+    const plugins = fs.readdirSync('./packages/').filter(
+      name => /^plugin-.*$/.test(name)
+    ).map(
+      name => /^plugin-(.*)$/[Symbol.match](name)[1]
+    )
+    await buildPlugins({
+      plugins,
+      clear: true, allFormat: true
+    })
+  }
+})().catch(console.error)
