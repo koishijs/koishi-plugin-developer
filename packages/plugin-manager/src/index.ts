62d24cca94c97bd888a3badfa56e52bdda908183
yijie4188 <yijie4188@gmail.com>
2021-05-08 14:51:10 +0800

    feat(plugin-manager): search plugin from remote

--- packages/plugin-manager/src/index.ts
+++ packages/plugin-manager/src/index.ts
@@ -45,108 +45,133 @@ export const apply = (ctx: Context, _config: Config = {}) => {
     'kpm <subCmd> [args...] 插件管理工具。', { authority: 4 }
   )
 
-  kpmCmd.subcommand('.install [...plugins]')
-    .alias('kpm.i')
-    .option('global', '-g 全局', { type: 'boolean' })
-    .action(async ({ session, options }, ...plugins) => {
-      let sessionCtx = ctx.select(
-        'userId', session.userId
-      )
-      if (options.global) { sessionCtx = ctx.app }
+  kpmCmd.subcommand(
+    '.install [...plugins]'
+  ).alias(
+    ...[ 'i', 'in', 'ins', 'inst', 'insta', 'instal', 'isnt', 'isnta', 'isntal', 'add' ].map(i => `kpm.${i}`)
+  ).option(
+    'global', '-g 全局', { type: 'boolean' }
+  ).action(async ({ session, options }, ...plugins) => {
+    let sessionCtx = ctx.select(
+      'userId', session.userId
+    )
+    if (options.global) { sessionCtx = ctx.app }
 
-      for (let i = 0; i < plugins.length; i++) {
-        const pluginName = '' + plugins[i]
-        const data = searchPlugin(pluginName)
+    for (let i = 0; i < plugins.length; i++) {
+      const pluginName = '' + plugins[i]
+      const data = searchPlugin(pluginName)
 
-        if (data !== null) {
-          const ctxPlugins = allPlugins.get(sessionCtx)
-          if (options.global) {
-            const plugins = allPlugins.plugins.filter(p => p.apply && p.apply === data.pluginModule.apply)
-            for (let j = 0; j < plugins.length; j++) {
-              await ctx.dispose(plugins[j])
-              const index = ctxPlugins.findIndex(val => val.plugin.apply === plugins[j].apply)
-              ctxPlugins.splice(index, 1)
-            }
-          }
-
-          const isInstalled = ctxPlugins && ctxPlugins.filter(
-            ctxPluginData => ctxPluginData.plugin?.apply && ctxPluginData.plugin?.apply === data.pluginModule?.apply
-          ).length >= 1
-          !isInstalled && sessionCtx.plugin(data.pluginModule)
-          await session.send(`installed ${pluginName}`)
-        } else {
-          await session.send(`本地未安装 ${pluginName} / koishi-plugin-${pluginName}`)
-        }
-      }
-      return '安装完成'
-    })
-
-  kpmCmd.subcommand('.uninstall [...plugins]')
-    .alias('kpm.uni').alias('kpm.un').alias('kpm.unlink')
-    .alias('kpm.remove').alias('kpm.rm').alias('kpm.r')
-    .option('global', '-g 全局', { type: 'boolean' })
-    .action(async ({ session, options }, ...plugins) => {
-      let sessionCtx = ctx.select(
-        'userId', session.userId
-      )
-      if (options.global) { sessionCtx = ctx.app }
-
-      for (let i = 0; i < plugins.length; i++) {
-        const pluginName = '' + plugins[i]
-        const data = searchPlugin(pluginName)
-
-        if (data !== null) {
-          const ctxPlugins = allPlugins.get(sessionCtx)
-          let plugins
-          if (options.global) {
-            plugins = allPlugins.plugins
-          } else {
-            plugins = allPlugins.get(sessionCtx).map(pluginData => pluginData.plugin)
-          }
-          plugins = plugins.filter(p => p.apply && p.apply === data.pluginModule.apply)
+      if (data !== null) {
+        const ctxPlugins = allPlugins.get(sessionCtx)
+        if (options.global) {
+          const plugins = allPlugins.plugins.filter(p => p.apply && p.apply === data.pluginModule.apply)
           for (let j = 0; j < plugins.length; j++) {
             await ctx.dispose(plugins[j])
             const index = ctxPlugins.findIndex(val => val.plugin.apply === plugins[j].apply)
-            if (index >= 0) {
-              ctxPlugins.splice(index, 1)
-              await session.send(`uninstalled ${pluginName}`)
-            }
+            ctxPlugins.splice(index, 1)
           }
-        } else {
-          await session.send(`本地未安装 ${pluginName} / koishi-plugin-${pluginName}`)
         }
-      }
-      return '卸载完成'
-    })
 
-  kpmCmd.subcommand('.list')
-    .alias('kpm.ls')
-    .option('remote', '-r 远程', { type: 'boolean' })
-    .option('global', '-g 全局', { type: 'boolean' })
-    .action(async ({ session, options }) => {
-      if (options.remote) {
-        const pluginsPagination = await npmApi.search('koishi-plugin')
-        let returnMsg = `共检索到: ${ pluginsPagination.total }个插件\n`
-        pluginsPagination.results.forEach(item => {
-          const pkg = item.package
-          returnMsg += `${ pkg.name }[${ pkg.author.name }]-${ pkg.version }\n` + pkg.description + '\n'
-        })
-        return returnMsg
+        const isInstalled = ctxPlugins && ctxPlugins.filter(
+          ctxPluginData => ctxPluginData.plugin?.apply && ctxPluginData.plugin?.apply === data.pluginModule?.apply
+        ).length >= 1
+        !isInstalled && sessionCtx.plugin(data.pluginModule)
+        await session.send(`installed ${pluginName}`)
+      } else {
+        await session.send(`本地未安装 ${pluginName} / koishi-plugin-${pluginName}`)
       }
-      let sessionCtx = ctx.select(
-        'userId', session.userId
-      )
-      if (options.global) { sessionCtx = ctx.app }
+    }
+    return '安装完成'
+  })
 
-      let pluginsList = ''
-      const ctxPlugins = allPlugins.get(
-        sessionCtx
-      )
-      ctxPlugins && ctxPlugins.forEach(ctxPluginData => {
-        const plugin = ctxPluginData.plugin
-        const name = plugin?.name ?? '未命名插件'
-        pluginsList += `[√] ${name}\n`
-      })
-      return pluginsList === '' ? '暂无已安装的插件' : pluginsList
+  kpmCmd.subcommand(
+    '.uninstall [...plugins]'
+  ).alias(
+    ...[ 'uni', 'un', 'unlink', 'remove', 'rm', 'r' ].map(i => `kpm.${i}`)
+  ).option(
+    'global', '-g 全局', { type: 'boolean' }
+  ).action(async ({ session, options }, ...plugins) => {
+    let sessionCtx = ctx.select(
+      'userId', session.userId
+    )
+    if (options.global) { sessionCtx = ctx.app }
+
+    for (let i = 0; i < plugins.length; i++) {
+      const pluginName = '' + plugins[i]
+      const data = searchPlugin(pluginName)
+
+      if (data !== null) {
+        const ctxPlugins = allPlugins.get(sessionCtx)
+        let plugins
+        if (options.global) {
+          plugins = allPlugins.plugins
+        } else {
+          plugins = allPlugins.get(sessionCtx).map(pluginData => pluginData.plugin)
+        }
+        plugins = plugins.filter(p => p.apply && p.apply === data.pluginModule.apply)
+        for (let j = 0; j < plugins.length; j++) {
+          await ctx.dispose(plugins[j])
+          const index = ctxPlugins.findIndex(val => val.plugin.apply === plugins[j].apply)
+          if (index >= 0) {
+            ctxPlugins.splice(index, 1)
+            await session.send(`uninstalled ${pluginName}`)
+          }
+        }
+      } else {
+        await session.send(`本地未安装 ${pluginName} / koishi-plugin-${pluginName}`)
+      }
+    }
+    return '卸载完成'
+  })
+
+  kpmCmd.subcommand(
+    '.list'
+  ).alias(
+    'kpm.ls'
+  ).option(
+    'global', '-g 全局', { type: 'boolean' }
+  ).action(async ({ session, options }) => {
+    let sessionCtx = ctx.select(
+      'userId', session.userId
+    )
+    if (options.global) { sessionCtx = ctx.app }
+
+    let pluginsList = ''
+    const ctxPlugins = allPlugins.get(
+      sessionCtx
+    )
+    ctxPlugins && ctxPlugins.forEach(ctxPluginData => {
+      const plugin = ctxPluginData.plugin
+      const name = plugin?.name ?? '未命名插件'
+      pluginsList += `[√] ${name}\n`
     })
+    return pluginsList === '' ? '暂无已安装的插件' : pluginsList
+  }).subcommand(
+    '.remote [query] 搜索远程依赖'
+  ).option(
+    'page', '-p [page] 当前页码', { type: 'number', fallback: 0 }
+  ).option(
+    'size', '-s [size] 当前个数', { type: 'number', fallback: 10 }
+  ).check(({ options }) =>
+    (options.size && !Number.isInteger(options.size)) ||
+    (options.page && !Number.isInteger(options.page))
+      ? '参数错误'
+      : undefined
+  ).action(async ({ options }, query = 'koishi-plugin') => {
+    const pluginsPagination = await npmApi.search(
+      query, options.page, options.size
+    )
+    let returnMsg = `共检索到: ${ pluginsPagination.total }个以来\n`
+    pluginsPagination.results.forEach(item => {
+      const pkg = item.package
+      returnMsg += `${ pkg.name }[${ pkg.author.name }]\n`
+      returnMsg += pkg.description + '\n'
+      returnMsg += `最新版本: ${pkg.version}\n`
+      returnMsg += '='.repeat(24) + '\n'
+    })
+    returnMsg += `当前位于 ${options.page} 页，共 ${
+      Math.ceil(pluginsPagination.total / options.size)
+    } 页`
+    return returnMsg
+  })
 }
