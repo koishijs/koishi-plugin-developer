770f6b6843506d1ffcc49773b1be37cbbdd86f74
yijie4188 <yijie4188@gmail.com>
2021-05-13 01:39:30 +0800

    chore(plugin-demo-view): restful api unit test

--- packages/plugin-demo-view/test/controllers/common.test.ts
+++ packages/plugin-demo-view/test/controllers/common.test.ts
@@ -13,20 +13,64 @@ app.plugin(require('koishi-plugin-demo-view'), {})
 
 axios.defaults.baseURL = `http://localhost:${ port }`
 
-before(async () => {
-  await app.start()
-})
-
 after(() => {
   process.exit()
 })
 
 describe('one', function () {
-  it('should return "some message."', async () => {
-    await expect(
-      axios.get('/demo-view/common/test')
-    ).to.eventually
-      .have.property('data')
-      .have.property('detail', 'some messages.')
+  before(async () => {
+    await app.start()
+  })
+
+  describe('basic test.', function () {
+    it('should return "some message."', async () => {
+      await expect(
+        axios.get('/demo-view/common/test')
+      ).to.eventually
+        .have.property('data')
+        .have.property('detail', 'some messages.')
+    })
+  })
+
+  describe('common support.', function () {
+    const u = {
+      id: '1001', auth: 4
+    }
+    before(async () => {
+      await app.database.initUser(u.id, u.auth)
+    })
+
+    it('should confirm whether there is a certain user.', async () => {
+      await expect(
+        axios.get(`/demo-view/common/mock/${ u.id }`)
+      ).to.eventually
+        .have.property('status', 200)
+
+      try {
+        await axios.get('/demo-view/common/mock/404')
+      } catch (e) {
+        expect(e.response).have.property('status', 404)
+      }
+    })
+
+    it('should return specify user data.', async () => {
+      await expect(
+        axios.get(`/demo-view/common/mock/${ u.id }?fields=id`)
+      ).to.eventually
+        .have.property('data')
+        .have.property('id', '1')
+
+      await expect(
+        axios.get(`/demo-view/common/mock/${ u.id }?fields=id,authority`)
+      ).to.eventually
+        .have.property('data')
+        .have.property('authority', 4)
+
+      await expect(
+        axios.get(`/demo-view/common/mock/${ u.id }?fields=undefined`)
+      ).to.eventually
+        .have.property('data')
+        .have.not.property('undefined')
+    })
   })
 })
