a4a8a3f34de76166ade10ff4e9084c2af064e3ab
yijie4188 <yijie4188@gmail.com>
2021-05-13 01:38:57 +0800

    feat(plugin-demo-view): add get platform user data

--- packages/plugin-demo-view/src/controllers/common.ts
+++ packages/plugin-demo-view/src/controllers/common.ts
@@ -1,5 +1,6 @@
 import { Context } from 'koishi-core'
 import Router from '@koa/router'
+import { User } from 'koishi'
 
 const prefix = 'common'
 
@@ -12,6 +13,27 @@ export const router = (ctx: Context): Router => {
     koaCtx.body = {
       detail: 'some messages.'
     }
+  })
+
+  router.get('/:platform/:uid', async koaCtx => {
+    const {
+      platform, uid
+    } = koaCtx.params as {
+      platform: User.Index, uid: string
+    }
+    const {
+      fields
+    } = koaCtx.query as {
+      fields: string
+    }
+
+    const user = await ctx.database.getUser(
+      platform, uid, [ 'id', ...(
+        (fields ?? '').split(',') as User.Index[]
+      ) ]
+    )
+    if (!user) throw new Error('[404]:用户不存在.')
+    koaCtx.body = user
   })
   return router
 }
