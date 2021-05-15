76c1ff7bbc912bb5e1ddc42bf9f54ac99f2d0544
yijie4188 <yijie4188@gmail.com>
2021-05-15 17:35:23 +0800

    chore(config.test): ignore system proxy

--- .mocharc.js
+++ .mocharc.js
@@ -1,9 +1,16 @@
 process.env.NODE_ENV = 'test'
 
+const proxy = {
+  http: '', https: ''
+}
+
+process.env.http_proxy = proxy.http
+process.env.https_proxy = proxy.https
+
 module.exports = {
   extension: [ 'ts', 'tsx' ],
   require: [
     'ts-node/register/transpile-only',
-    'tsconfig-paths/register',
+    'tsconfig-paths/register'
   ]
 }
