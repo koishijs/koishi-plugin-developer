992a7d79a70ae1bbe9b1d69b6569781e36f54c68
yijie4188 <yijie4188@gmail.com>
2021-05-19 01:25:51 +0800

    feat(plugin-manager): install plugins from remote in service

--- packages/plugin-manager/src/services/plugin.ts
+++ packages/plugin-manager/src/services/plugin.ts
@@ -1,6 +1,8 @@
 import glob from 'glob'
 import path from 'path'
 import fs from 'fs'
+import { npmApi } from '../core/NpmApi'
+import { doCommand } from 'koishi-plugin-manager'
 
 export type Package = {
   name: string
@@ -28,7 +30,35 @@ export const pluginService = {
     })
     return plugins
   },
-  installPlugins() {
-    // TODO install plugins
+  async installPlugins(plugins: string[]): Promise<string[]> {
+    const localPkgs = pluginService.localPlugins()
+    const waitInstallPlugins = []
+    for (let i = 0; i < plugins.length; i++) {
+      const pluginName = plugins[i]
+      try {
+        if (localPkgs.findIndex(pkg => pkg.name === pluginName) == -1) {
+          const pkgData = await npmApi.get(pluginName)
+          const pkg = pkgData.collected.metadata
+          waitInstallPlugins.push(pkg.name)
+        }
+      } catch (e) {
+        if (e.message === 'Request failed with status code 404') {
+          throw new Error(`远程不存在 ${ pluginName }。`)
+        }
+      }
+    }
+    if (waitInstallPlugins.length === 0) return []
+
+    const args = []
+    const absPath = path.resolve(
+      process.cwd(), './package.json'
+    )
+    const pkg = JSON.parse(fs.readFileSync(absPath).toString()) as Package
+
+    if (pkg.workspaces) {
+      args.push('-W')
+    }
+    await doCommand('yarn', [ 'add', ...args, ...waitInstallPlugins ])
+    return waitInstallPlugins
   }
 }
