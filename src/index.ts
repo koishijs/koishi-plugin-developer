12ab5831abf332ad76f45a8e4299cb8647534750
yijie4188 <yijie4188@gmail.com>
2021-08-06 13:36:19 +0800

    refactor: use text dialogue plugin as test adapter

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
+import path from 'path'
 import { App as KoishiApp } from 'koishi'
 
+import 'koishi-plugin-text-dialogue'
+
 const app = new KoishiApp({
-  bots: []
+  bots: [{
+    type: 'text-dialogue',
+    selfId: 'second-jie',
+    watchOptionsMap: [path.resolve(__dirname, '.temp.chat.md')]
+  }]
 })
 
 app.start().then()
