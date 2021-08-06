c6622fdb6ae43f480ceb63ab636766383c1bb383
yijie4188 <yijie4188@gmail.com>
2021-08-06 21:48:32 +0800

    feat(plugin-text-dialogue): supplement unit test

--- packages/plugin-text-dialogue/test/index.test.ts
+++ packages/plugin-text-dialogue/test/index.test.ts
@@ -1,10 +1,13 @@
-import { App } from 'koishi'
-import { expect } from 'chai'
 import fs from 'fs'
 import path from 'path'
+import { expect } from 'chai'
+import qface from 'qface'
+import { App, segment } from 'koishi'
 
 import 'koishi-plugin-text-dialogue'
 import { analyzeMessage } from 'koishi-plugin-text-dialogue'
+import Identicon from 'identicon.js'
+import md5 from 'js-md5'
 
 after(() => {
   process.exit()
@@ -62,21 +65,73 @@ describe('Text Dialogue Plugin', () => {
       await app.start()
     })
 
-    it('should hello bot', (done) => {
+    it('should watch bot say hello.', (done) => {
       app.once('message', async session => {
         if (session.content === 'hello world') {
           await session.send('hello master')
-          expect(fs.readFileSync(watchFile).toString())
-            .to.be.eq('' +
-              '> yijie@hello world<\n' +
-              '> test@hello master\n' +
-              '> '
-            )
-          done()
         }
       })
 
       fs.writeFileSync(watchFile, '> yijie@hello world<')
+      setTimeout(() => {
+        expect(fs.readFileSync(watchFile).toString())
+          .to.be.eq('' +
+          '> yijie@hello world<\n' +
+          '> test@hello master\n' +
+          '> ')
+        done()
+      }, 100)
+    })
+
+    it('should get username.', (done) => {
+      app.command('whoami', 'tell me who am I.')
+        .action(async ({ session }) => {
+          return `you are ${session.username}`
+        })
+      fs.writeFileSync(watchFile, '> yijie@whoami<')
+      setTimeout(() => {
+        expect(fs.readFileSync(watchFile).toString())
+          .to.be.eq('' +
+          '> yijie@whoami<\n' +
+          '> test@you are yijie\n' +
+          '> ')
+        done()
+      }, 100)
+    })
+
+    it('should show right base 64 image.', (done) => {
+      app.command('show-me-avatar', 'show me avatar.')
+        .action(async ({ session }) => {
+          const base64Str = new Identicon(md5(session.username), 64).toString()
+          return `you avatar is \n${segment.image(`base64://${base64Str}`)}`
+        })
+      fs.writeFileSync(watchFile, '> yijie@show-me-avatar<')
+      setTimeout(() => {
+        const base64Str = new Identicon(md5('yijie'), 64).toString()
+        expect(fs.readFileSync(watchFile).toString())
+          .to.be.eq('' +
+          '> yijie@show-me-avatar<\n' +
+          '> test@you avatar is \n' +
+          `> ![image](data:image/png;base64,${base64Str})\n` +
+          '> ')
+        done()
+      }, 100)
+    })
+
+    it('should return qq face image gif.', (done) => {
+      app.command('show-qq-face <id>', 'show a qq face.')
+        .action(async (_, id) => {
+          return segment('face', { id })
+        })
+      fs.writeFileSync(watchFile, '> yijie@show-qq-face 1 <')
+      setTimeout(() => {
+        expect(fs.readFileSync(watchFile).toString())
+          .to.be.eq('' +
+          '> yijie@show-qq-face 1 <\n' +
+          `> test@![QQFace 1](${qface.getUrl(1)})\n` +
+          '> ')
+        done()
+      }, 100)
     })
   })
 })
