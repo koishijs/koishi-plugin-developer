e84dc7a42c3af077ee2ee26597177e99309c7c4c
yijie4188 <yijie4188@gmail.com>
2022-10-27 15:04:07 +0800

    feat(cli): auto reload plugin

--- packages/cli/lib/koishi-hot-reload.js
+++ packages/cli/lib/koishi-hot-reload.js
@@ -1,10 +1,67 @@
-const Cordis = require('cordis')
+const Koishi = require('koishi')
 
-const originalPlugin = Cordis.Context.prototype.plugin
-Cordis.Context.prototype.plugin = function (...args) {
-  const { name } = args[0]
-  if (name) {
-    process.send?.(`installed ${name}`)
-  }
-  return originalPlugin.apply(this, args)
+/** @typedef {import('./protocol').Protocol} Protocol */
+
+/**
+ * @template {Protocol.UpTypes} T
+ * @param {T} type
+ * @param {Protocol.UpDispatcherMap[T][0]} data
+ * @returns {{type, data}}
+ */
+function dispatch(type, data) {
+  return { type, data }
 }
+
+/** @typedef {{
+  ctx: Koishi.Context;
+  name: string;
+  fork: Koishi.Context;
+  config: any;
+}} CtxRelative */
+
+/** @type {Map<string, CtxRelative[]>} */
+const ctxMap = new Map()
+
+const originalPlugin = Koishi.Context.prototype.plugin
+Koishi.Context.prototype.plugin = function (...args) {
+  const [plugin, config] = args
+  const { name } = plugin
+  if (['runtime', 'validate'].includes(name))
+    return
+
+  const fork = originalPlugin.apply(this, args)
+
+  try {
+    const resolver = require.resolve(`koishi-plugin-${name}`)
+    const relative = { ctx: this, name, fork, config }
+    if (ctxMap.has(resolver)) {
+      ctxMap.get(resolver).push(relative)
+    } else {
+      ctxMap.set(resolver, [relative])
+    }
+    if (name) {
+      process.send?.(dispatch('plugin:apply', {
+        name, config, resolver
+      }))
+    }
+  } catch (e) {
+    console.error(e)
+  }
+  return fork
+}
+
+process.on(
+  'message',
+  /** @param {Protocol.DownDispatcher[0]} msg */ function (msg) {
+    if (msg.type === 'plugin:reload') {
+      const resolver = msg.data
+      const relatives = ctxMap.get(resolver)
+      relatives.forEach(({ ctx, name, fork, config }) => {
+        console.log(`[koishi] disposing plugin ${name}`)
+        fork.dispose()
+        delete require.cache[resolver]
+        console.log(`[koishi] disposed plugin ${name}`)
+        ctx.plugin(require(resolver), config)
+      })
+    }
+  })
--- packages/cli/src/start.ts
+++ packages/cli/src/start.ts
@@ -1,7 +1,47 @@
 import type { Command } from 'commander'
+import chokidar from 'chokidar'
 import fs from 'fs'
 import path from 'path'
+import { ChildProcessWithoutNullStreams } from 'child_process'
 import { doCommand } from './utils'
+import type { Protocol } from '../lib/protocol'
+
+namespace Processes {
+  let cmd: ChildProcessWithoutNullStreams | null = null
+  let fsWatcher: chokidar.FSWatcher | null = null
+  const resolverDirMap = new Map<string, string>()
+  const initFSWatcher = (botName: string) => {
+    fsWatcher = chokidar.watch(`./bots/${botName}`)
+    fsWatcher.on('change', filePath => {
+      const resolvers = new Set<string>()
+      resolverDirMap.forEach((dir, resolver) => {
+        if (filePath.startsWith(dir)) {
+          resolvers.add(resolver)
+        }
+      })
+      resolvers.forEach(resolver => {
+        cmd?.send({ type: 'plugin:reload', data: resolver }, err => {
+          err && console.error(err)
+        })
+      })
+    })
+  }
+  export const attach = (botName: string, attachCmd: ChildProcessWithoutNullStreams) => {
+    initFSWatcher(botName)
+
+    cmd = attachCmd
+    cmd.on('message', (dispatch: Protocol.UpDispatcher[0]) => {
+      const data = dispatch.data
+
+      switch (dispatch.type) {
+        case 'plugin:apply':
+          resolverDirMap.set(data.resolver, path.dirname(data.resolver))
+          fsWatcher.add(resolverDirMap.get(data.resolver))
+          break
+      }
+    })
+  }
+}
 
 export function apply(program: Command) {
   program
@@ -30,6 +70,6 @@ export function apply(program: Command) {
         ...opts,
         // @ts-ignore
         stdio: [null, null, null, 'ipc'],
-      })
+      }, cmd => Processes.attach(botName, cmd))
     })
 }
